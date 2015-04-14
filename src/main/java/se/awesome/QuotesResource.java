bc653528b73a83587430119a98ecce24db38b808
andberg <andreaberglund@hotmail.com>
2015-04-14 13:18:04 +0200

    git it to work again an across origin!

--- src/main/java/se/awesome/QuotesResource.java
+++ src/main/java/se/awesome/QuotesResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 //http://localhost:8080/crazy-quotes/quotes
 
@@ -11,7 +12,10 @@ import javax.ws.rs.core.MediaType;
 public class QuotesResource {
 	@GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public Response getIt() {
+        return Response.status(200).header("Access-Control-Allow-Origin", "*")
+        		.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        		.entity("test") 
+        		.build(); 
     }
 }
