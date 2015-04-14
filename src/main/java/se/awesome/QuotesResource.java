7de8f9c36a8930f8fcbc84c7dd0920d8a40e696a
andberg <andreaberglund@hotmail.com>
2015-04-14 15:22:42 +0200

    Added enteties and trying to get the Post from front-end to work

--- src/main/java/se/awesome/QuotesResource.java
+++ src/main/java/se/awesome/QuotesResource.java
@@ -1,6 +1,10 @@
 package se.awesome;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,12 +14,24 @@ import javax.ws.rs.core.Response;
 
 @Path("/quotes")
 public class QuotesResource {
+	
+	List<String> listOfInput = new ArrayList<String>(); 
+	
 	@GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response getIt() {
         return Response.status(200).header("Access-Control-Allow-Origin", "*")
         		.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-        		.entity("test") 
+        		.entity(listOfInput.toString()) 
         		.build(); 
     }
+	
+	@POST
+	public Response addInput(String input){
+		System.out.println(input);
+		listOfInput.add(input.toString()); 
+		return Response.status(201).header("Access-Control-Allow-Origin", "*")
+        		.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        		.build(); 
+	}
 }
